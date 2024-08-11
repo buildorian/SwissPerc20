1c97c39829274fe4a551b9bb94d88f8d8a0214fe
buildorian buildoor <aptosdotbuild@gmail.com>
2024-08-11 12:20:21 +0000

    Initial commit

---
+++ scripts/alifBal.js
@@ -0,0 +1,27 @@
+const hre = require("hardhat");
+  const { sendSignedShieldedQuery } = require("./utils");
+  const ethers = require("ethers");
+  
+  const PK = "0x56c44ac5cd895a250ef38c7e925269d44e6861c95a80ea71f65cc7d46e6f1d57";
+  const deployedContractAddress = "0x875C221149F5C9eC761adE9a6CbD8aAcC76D5D8D";
+  
+  async function main() {
+    const PERC20 = await hre.ethers.getContractFactory("PERC20Sample");
+    const perc20 = PERC20.attach(deployedContractAddress);
+  
+    const provider = new hre.ethers.providers.JsonRpcProvider(hre.network.config.url);
+    const wallet = new hre.ethers.Wallet(PK, provider);
+    // let addy2 = "0x16af037878a6cAce2Ea29d39A3757aC2F6F7aac1";
+    // addy2 = ethers.utils.getAddress(addy2);
+  
+    let encodedFunctionData = perc20.interface.encodeFunctionData("balanceOf", [wallet.address]);
+    let req = await sendSignedShieldedQuery( wallet, perc20.address, encodedFunctionData);
+  
+    let balance = perc20.interface.decodeFunctionResult("balanceOf", req)[0];
+    console.log('Balance: ', balance.toString());
+  
+  }
+  main().catch((error) => {
+    console.error(error);
+    process.exitCode = 1;
+  });
